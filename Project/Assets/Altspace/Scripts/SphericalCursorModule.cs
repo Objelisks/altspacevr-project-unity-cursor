966615e639f737481679187d8bdcabcc3a4ee048
Objelisks <supertyrian@gmail.com>
2015-07-28 12:40:10 -0700

    Finished Step 1 changes

--- Project/Assets/Altspace/Scripts/SphericalCursorModule.cs
+++ Project/Assets/Altspace/Scripts/SphericalCursorModule.cs
@@ -6,7 +6,7 @@ public class SphericalCursorModule : MonoBehaviour {
 
 	// This is a scale factor that determines how much to scale down the cursor based on its collision distance.
 	public float DistanceScaleFactor;
-	
+
 	// This is the layer mask to use when performing the ray cast for the objects.
 	// The furniture in the room is in layer 8, everything else is not.
 	private const int ColliderMask = (1 << 8);
@@ -24,30 +24,49 @@ public class SphericalCursorModule : MonoBehaviour {
 	private const float MaxDistance = 100.0f;
 
 	// Sphere radius to project cursor onto if no raycast hit.
-	private const float SphereRadius = 1000.0f;
+	private const float SphereRadius = 10.0f;
 
-    void Awake() {
+	// Spherical coordinates to update each frame.
+	private Vector2 cursorSphericalCoords = new Vector2(0.0f, 0.0f);
+
+  void Awake() {
 		Cursor = transform.Find("Cursor").gameObject;
 		CursorMeshRenderer = Cursor.transform.GetComponentInChildren<MeshRenderer>();
-        CursorMeshRenderer.renderer.material.color = new Color(0.0f, 0.8f, 1.0f);
-    }	
+    CursorMeshRenderer.GetComponent<Renderer>().material.color = new Color(0.0f, 0.8f, 1.0f);
+  }
 
 	void Update()
 	{
-		// TODO: Handle mouse movement to update cursor position.
+		// Handle mouse movement to update cursor position.
+		float horizontal = Input.GetAxis("Mouse X");
+		float vertical = Input.GetAxis("Mouse Y");
 
-		// TODO: Perform ray cast to find object cursor is pointing at.
-		// TODO: Update cursor transform.
-		var cursorHit = new RaycastHit();/* Your cursor hit code should set this properly. */;
+		cursorSphericalCoords.x += horizontal * Sensitivity;
+		cursorSphericalCoords.y += vertical * Sensitivity;
 
+		// Start ray cast from camera position, head in direction specified by spherical coordinates
+		Vector3 origin = transform.position;
+		Vector3 direction = Quaternion.AngleAxis(cursorSphericalCoords.x, Vector3.up)
+											* Quaternion.AngleAxis(cursorSphericalCoords.y, Vector3.left)
+											* Vector3.forward;
+
+		// Perform ray cast to find object cursor is pointing at.
+		RaycastHit cursorHit = new RaycastHit();
+		bool foundHit = Physics.Raycast(origin, direction, out cursorHit, MaxDistance, ColliderMask);
+
+		// Update cursor transform.
 		// Update highlighted object based upon the raycast.
-		if (cursorHit.collider != null)
-		{
+		if (foundHit && cursorHit.collider != null) {
+			float distanceToObject = cursorHit.distance;
+			float scale = (distanceToObject * DistanceScaleFactor + 1.0f) / 2.0f;
+			
 			Selectable.CurrentSelection = cursorHit.collider.gameObject;
-		}
-		else
-		{
+			Cursor.transform.position = cursorHit.point;
+			Cursor.transform.localScale = new Vector3(scale, scale, scale);
+		} else {
 			Selectable.CurrentSelection = null;
+			Cursor.transform.localPosition = direction * SphereRadius;
+			Cursor.transform.localScale = DefaultCursorScale;
 		}
 	}
 }
---
+++ Project/Assets/CursorShader.shader
@@ -0,0 +1,41 @@
+﻿Shader "Custom/CursorShader" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+	}
+	SubShader {
+		Tags {"Queue"="Transparent" "IgnoreProjector"="True" "RenderType"="Transparent"}
+		LOD 200
+
+		ZTest Always
+		ZWrite On
+
+		CGPROGRAM
+
+		#pragma surface surf Flat
+
+		sampler2D _MainTex;
+
+		half4 LightingFlat (SurfaceOutput s, half3 lightDir, half atten) {
+			half4 c;
+			c.rgb = s.Albedo;
+			c.a = s.Alpha;
+			return c;
+		}
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		fixed4 _Color;
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			// Albedo comes from a texture tinted by color
+			fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+			o.Albedo = c.rgb;
+			o.Alpha = 1.0;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
